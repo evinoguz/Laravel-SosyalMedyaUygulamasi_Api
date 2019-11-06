b3139f0e399b60abb3bb6865000b92aa4b6cfe63
SergenKoca <sergenkoca@outlook.com.tr>
2019-11-06 15:56:32 +0300

    Like and Comment Controller

---
+++ app/Http/Controllers/Api/LikeController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    public function create(Request $request){
+        $user = Auth::user();
+        if($user!=null){
+            $like = new LikeModel();
+            $like->post_id = $request->id;
+            $like->user_id = $user->id;
+            $like->save();
+            return response()->json([
+                'like'=>$like,
+                'user'=>$user
+            ]);
+        }
+        return response()->json([
+            'error'=>'Unauthorised'
+        ], 401);
+    }
+
+    public function getLike(Request $request){
+        $user = Auth::user();
+        if($user!=null){
+            $like = $like = LikeModel::where('post_id',$request->id)->where('user_id',$user->id)->first();
+            if($like!=null){
+                return response()->json([
+                    'message'=>'you liked this post',
+                    'like'=>$like,
+                    'user'=>$user
+                ]);
+            }
+            else{
+                return response()->json([
+                    'message'=>'you have no like at this post',
+                    'user'=>$user
+                ]);
+            }
+        }
+        return response()->json([
+            'error'=>'Unauthorised'
+        ], 401);
+    }
+
+    public function delete(Request $request){
+        $user = Auth::user();
+        if($user!=null){
+            $like = LikeModel::where('post_id',$request->id)->where('user_id',$user->id)->first();
+            if($like !=null){
+                $like->delete();
+                return response()->json([
+                    'message'=>'like was removed',
+                    'user'=>$user
+                ]);
+            }
+            else{
+                return response()->json([
+                    'message'=>'you have no like at this post',
+                    'user'=>$user
+                ]);
+            }
+
+        }
+        return response()->json([
+            'error'=>'Unauthorised'
+        ], 401);
+    }
+}
