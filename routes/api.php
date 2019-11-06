548554a95e581a8491d42032744806cf05d38390
evinoguz <36725347+evinoguz@users.noreply.github.com>
2019-11-06 16:28:21 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,48 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//
+//});
+
+
+Route::group(['middleware' => 'api'], function () {
+
+    Route::group(['prefix' => 'auth'], function () {
+        Route::get('profile/{id}','Api\ProfileController@profile');
+        Route::post('post','Api\PostController@post');
+        Route::get('follow_follower','Api\Follow_FollowerController@follow_follower');
+
+    });
+
+    Route::group(['prefix' => 'profile'], function () {
+
+    });
+
+    Route::group(['prefix' => 'post'], function () {
+
+    });
+
+    Route::group(['prefix' => 'follow_follower'], function () {
+
+    });
+
+    Route::group(['prefix' => 'message'], function () {
+
+    });
+
+    Route::group(['prefix' => 'like'], function () {
+
+    });
+
+    Route::group(['prefix' => 'notification'], function () {
+
+    });
+
+    Route::group(['prefix' => 'comment'], function () {
+
+    });
 
 });
 
