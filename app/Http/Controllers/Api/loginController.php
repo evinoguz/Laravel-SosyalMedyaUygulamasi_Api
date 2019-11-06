e40bd13e6ccfdb033965a2b5f4ba3ea7a3c3171c
SergenKoca <sergenkoca@outlook.com.tr>
2019-11-06 17:41:14 +0300

    Login and Register Controller

--- app/Http/Controllers/Api/loginController.php
+++ app/Http/Controllers/Api/loginController.php
@@ -8,6 +8,19 @@ use Illuminate\Http\Request;
 class loginController extends Controller
 {
    public function login(){
-       deneme yazi
+       if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+           $user = Auth::user();
+           $token =  $user->createToken('MyApp')-> accessToken;
+
+           return response()->json([
+               'token' => $token,
+               'user'=>$user
+           ], $this-> successStatus);
+       }
+       else{
+           return response()->json([
+               'error'=>'Unauthorised'
+           ], 401);
+       }
    }
 }
