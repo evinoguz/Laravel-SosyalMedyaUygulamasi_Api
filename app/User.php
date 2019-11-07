a8ccd33303edd280ddd8bd3c85e306f964108b69
sevgikaraagac <57255817+sevgikaraagac@users.noreply.github.com>
2019-11-07 17:01:21 +0300

    passport commit

--- app/User.php
+++ app/User.php
@@ -5,9 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens, Notifiable;
+
     use Notifiable;
 
     /**
