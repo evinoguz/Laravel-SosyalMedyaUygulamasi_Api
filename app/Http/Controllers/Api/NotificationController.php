fe268349308c9efd1f8b35a9313cd4da968903fe
sevgikaraagac <57255817+sevgikaraagac@users.noreply.github.com>
2019-11-07 12:28:22 +0300

    notification commit

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -3,9 +3,43 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Notification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NotificationController extends Controller
 {
-    //
+    public function remove_notification(Request $request){
+        $user = Auth::user();
+        $notification = Notification::where('id', $request->id)->first();
+        if($user!=null) {
+            $notification->delete();
+            return response()->json([
+                'message' => 'notification removed',
+                'user' => $user
+            ]);
+        }
+        else{
+            return response()->json([
+                'error'=>'Unauthorised'
+            ]);
+        }
+    }
+
+    public function get_notification(Request $request)
+    {
+        $user = Auth::user();
+
+        $notification = Notification::where('id', $request->id)->first();
+        if ($user != null) {
+            return response()->json([
+                'message' => $notification,
+                'user' => $user,
+            ]);
+        } else {
+            return response()->json([
+                'error' => 'Unauthorised'
+            ]);
+        }
+    }
 }
