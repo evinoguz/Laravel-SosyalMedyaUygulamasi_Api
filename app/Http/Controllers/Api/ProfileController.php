54a0aaa4f18a80409d3e726e8a76ad872e68a572
evinoguz <36725347+evinoguz@users.noreply.github.com>
2019-11-11 15:11:24 +0300

    profile create

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use App\Models\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,50 +13,88 @@ use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {
-    public function get_profile(Request $request)
+    public function create(Request $request)
     {
-        $user=Auth::user();
-        $profile=Profile::find($request->id);
-        if($user==null)
-        return Response::json([
-            'message'=>'Not active users',
-        ]);
+        $user =auth('api')->user();
 
-        else
-        {
-            return Response()->json([
-                'id'=>$profile->id,
-                'user_id'=>$user->id,
-                'img_url'=>$profile->img_url,
-                'biography'=>$profile->biography,
+        if ($user != null) {
+            $validator = Validator::make($request->all(), [
+                'img_url'=>'',
+                'biography'=>'',
+
             ]);
-        }
+            if($validator->fails())
+            {
+                $errors=array();
+                $i=0;
+                foreach ($validator -> errors()->messages() as $key=>$value)
+                {
+                    $i+=1;
+                    $errors[]=[
+                        'field'=>$key,
+                        $i.'.'.'messages'=>$value[0],
+                    ];
+                }
+                return Response::json([
+                    "result"=>"error",
+                    "errors"=>$errors,
+                ]);
+            }
 
-    }
-    public function edit_profile(Request $request)
-    {
-        $user=Auth::user();
-        $profile=Profile::find($request->id);
-        if($user==null)
-            return Response::json([
-                'message'=>'Not active users',
-            ]);
-
-        else
-        {
-            $profile->id=$request->id;
+            $profile=new Profile();
             $profile->user_id=$user->id;
-            $profile->img_url=$request->img_url;
-            $profile->biography=$request->biography;
-            $profile->save();
+            if($request->has('img_url')){
+                $file=$request->img_url;
+                $file->move(public_path().'/profile/',$file->getClientOriginalName());
+                $profile->img_url='/profile/'.$file->getClientOriginalName();
 
-            return Response()->json([
-                'id'=>$request->id,
-                'user_id'=>$user->id,
-                'img_url'=>$request->img_url,
-                'biography'=>$request->biography,
+            }
+            $profile->biography=$request->biography;
+            $profile->saveOrFail();
+            return Response::json([
+                "result" => "ok",
+                "message" => "Kayit Basarili",
             ]);
         }
+        return Response::json([
+            'error' => 'Unauthorised'
+        ], 401);
+    }
 
+
+    public function updateProfile(Request $request)
+    {
+        $user = auth('api')->user();
+        if ($user == null)
+            return Response::json([
+                'message' => 'Not active users',
+            ], 401);
+        else {
+            $profile = Profile::where('user_id',$user->id)->first();
+            if ($profile == null)
+                return Response::json([
+                    'message' => 'There is not user',
+                ]);
+            else {
+                    $profile->user_id = $user->id;
+                    if ($request->has('img_url')) {
+                        if (file_exists(public_path() . $profile->img_url)) {
+                            unlink(public_path() . $profile->img_url);
+                        }
+                        $file = $request->img_url;
+                        $file->move(public_path() . '/profile/', $file->getClientOriginalName());
+                        $profile->img_url = '/profile/' . $file->getClientOriginalName();
+                    }
+                    $profile->biography=$request->biography;
+                    $profile->saveOrFail();
+
+                    return Response::json([
+                        "result" => "ok",
+                        "message" => "Kayit Güncellendi",
+                        $profile,
+                    ]);
+
+            }
+        }
     }
 }
